0eae7476bd232e46a55fa84f6f3d6ac4765a14f2
luj8484-svg <luj8484@gmail.com>
2026-05-27 00:22:47 +0800

    Add Guangzhou hotel area guide for Canton Fair buyers

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,6 +6,7 @@ const blog = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
+    metaTitle: z.string().optional(),
     description: z.string(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
